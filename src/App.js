7abd72b562142e7d504b9bfb88b33729d378219b
farhan-sadiq-mahi <fsmahi100@gmail.com>
2022-11-29 11:37:16 +0600

    'complete'

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import Filters from "./components/Filters";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Rentals from "./components/Rentals";
 
 function App() {
   return (
-    <div className="App">
-      <h1 className='text-3xl text-red-500'>welcome to my airbnb</h1>
+    <div className="">
+      {/* Navbar */}
+      <Navbar />
+      {/* Filters */}
+      <div className="sm:mx-6 md:mx-10 lg:mx-12 px-3">
+        <Filters />
+        {/* Rentals */}
+        <Rentals />
+      </div>
+
+      {/* Footer */}
+      <Footer />
     </div>
   );
 }
